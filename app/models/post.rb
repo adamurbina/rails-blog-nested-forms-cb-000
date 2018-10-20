0544202e8630106ab42d2a7035e5718321b8cd1a
Adam Urbina <adamurbina@gmail.com>
2018-10-20 11:12:50 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,13 +5,13 @@ class Post < ActiveRecord::Base
   has_many :tags, :through => :post_tags
 
   validates_presence_of :name, :content
-  accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+  #accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
 
-  # def tags_attributes=(attribute_hash)
-  #   if attribute_hash.present?
-  #     tag = Tag.find_or_create_by(name: attribute_hash["name"])
-  #     self.tags << tag
-  #   end
-  # end
+  def tags_attributes=(attribute_hash)
+    if attribute_hash.present? && !attribute_hash["name"].blank?
+      tag = Tag.find_or_create_by(name: attribute_hash["name"])
+      self.tags << tag
+    end
+  end
 
 end
